c8821e593f02a1743fa964af32006afbd557c8a7
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-08 16:16:58 +0300

    starting the one blood app

---
+++ app/src/main/java/com/example/oneblood/screens/Splash.kt
@@ -0,0 +1,27 @@
+package com.example.oneblood.scre
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun Splash(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.background)){
+
+            
+        }
+    }
+
+}
+
