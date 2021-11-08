6c7a3e93dbd55912f17d9f533cb1f5468df69900
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-08 17:07:48 +0300

    changed the statusbar color for the theme

--- app/src/main/java/com/example/oneblood/ui/theme/Color.kt
+++ app/src/main/java/com/example/oneblood/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.example.oneblood.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Purple500 = Color(0xFFDDD8E6)
+val Purple700 = Color(0x5B3700B3)
 val Teal200 = Color(0xFF03DAC5)
