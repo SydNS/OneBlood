dfc52b2174fe4feacf0e99ee255095d44e2d47e2
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-08 16:26:07 +0300

    added the app logo

--- app/src/main/java/com/example/oneblood/screens/Splash.kt
+++ app/src/main/java/com/example/oneblood/screens/Splash.kt
@@ -1,5 +1,6 @@
 package com.example.oneblood.scre
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -8,8 +9,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-
+import com.example.oneblood.R
 
 
 @Preview(showBackground = true)
@@ -18,10 +21,12 @@ fun Splash(){
     Box(modifier = Modifier
         .fillMaxSize()
         .background(MaterialTheme.colors.background)){
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Splash image"
+        ,contentScale = ContentScale.Inside)
 
             
         }
-    }
+
 
 }
 
