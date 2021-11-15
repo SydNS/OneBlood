915698d7f07549ceca546d86feba5639341325ba
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-16 02:07:44 +0300

    added signup button for social login with!!!

--- app/src/main/java/com/example/oneblood/screens/Login.kt
+++ app/src/main/java/com/example/oneblood/screens/Login.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,6 +35,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.oneblood.R
@@ -47,92 +52,95 @@ fun LoginScreen() {
 
     ) {
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentHeight(align = Alignment.CenterVertically)
-                .padding(20.dp, 40.dp)
-                .background(
-                    color = Color.Red, shape = RoundedCornerShape(
-                        250.dp,
-                        0.dp, 250.dp, 250.dp
-                    )
-                )
-
-        ) {
-
-
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Splash image",
-                contentScale = ContentScale.Inside,
-                modifier = Modifier
-                    .height(300.dp)
-                    .width(300.dp)
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .graphicsLayer(alpha = 5F)
-            )
-
-            OutlinedTextField(
-                value = "",
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color.Red,
-                    focusedLabelColor = Color.Red,
-                    cursorColor = Color.Red
-                ),
-                placeholder = { Text(text = "Email Address") },
-                textStyle = TextStyle(color = Color.White),
-                onValueChange = {},
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(0.dp, 5.dp),
-                label = { Text(text = "Email Address") },
-
-                singleLine = true
-            )
-            OutlinedTextField(
-                value = "",
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color.Red,
-                    focusedLabelColor = Color.Red,
-                    cursorColor = Color.Red
-                ),
-                placeholder = { Text(text = "Password") },
-                onValueChange = {},
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(0.dp, 5.dp),
-                label = { Text(text = "Password") },
-
-                singleLine = true
-            )
-
-            OutlinedButton(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Blue,
-                ),
-                modifier = Modifier
-                    .padding(0.dp, 10.dp)
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .width(280.dp)
-                    .height(50.dp),
-
-                ) {
-                Text(
-                    text = stringResource(id = R.string.login), color = Color.White
-                )
-
-            }
-
-        }
-
+        LoginContent()
 
 
 
     }
 
 
+}
+
+@Composable
+fun LoginContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentHeight(align = Alignment.CenterVertically)
+            .padding(20.dp,50.dp)
+            .background(
+                color = Color.Red, shape = RoundedCornerShape(
+                    250.dp,
+                    0.dp, 250.dp, 250.dp
+                )
+            )
+
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Splash image",
+            contentScale = ContentScale.Inside,
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+                .graphicsLayer(alpha = 5F)
+        )
+
+        OutlinedTextField(
+            value = "",
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Red,
+                focusedLabelColor = Color.Red,
+                cursorColor = Color.Red
+            ),
+            placeholder = { Text(text = "Email Address") },
+            textStyle = TextStyle(color = Color.White),
+            onValueChange = {},
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(0.dp, 5.dp),
+            label = { Text(text = "Email Address") },
+
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = "",
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Red,
+                focusedLabelColor = Color.Red,
+                cursorColor = Color.Red
+            ),
+            placeholder = { Text(text = "Password") },
+            onValueChange = {},
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(0.dp, 5.dp),
+            label = { Text(text = "Password") },
+
+            singleLine = true
+        )
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Blue,
+            ),
+            modifier = Modifier
+                .padding(0.dp, 10.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+                .width(280.dp)
+                .height(50.dp),
+
+            ) {
+            Text(
+                text = stringResource(id = R.string.login), color = Color.White
+            )
+
+        }
+
+    }
 }
 
 @Preview
@@ -150,15 +158,63 @@ fun SignIn() {
 
 @Preview
 @Composable
-fun UsefulLinks(){
-    
+fun UsefulLinks() {
+    Column(modifier = Modifier.wrapContentHeight()) {
+        Registerlink()
+        SocialRegister()
+    }
+
+}
+
+@Preview
+@Composable
+fun Registerlink() {
+    Text(
+        text = stringResource(id = R.string.registerLink),
+        fontStyle = FontStyle(R.font.cavdreams),
+        textAlign = TextAlign.Center
+    )
+    Divider(color = Color.Blue, thickness = 2.dp)
+
+}
+
+@Preview
+@Composable
+fun SocialRegister() {
+    Row {
+        GoogleSignUp()
+        FacebookSignUp()
+
+    }
+}
+@Preview
+@Composable
+fun FacebookSignUp() {
+    // Icon Button
+    // Icon on the left of text
+    Button(onClick = {}, modifier = Modifier.padding(8.dp)) {
+        Row {
+            Icon(
+                imageVector = Icons.Filled.Phone,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 4.dp)
+            )
+            Text(text = "Phonenumber")
+        }
+    }
 
 }
 @Preview
 @Composable
-fun Registerlink(){
-    Text(text = stringResource(id = R.string.registerLink),
-        fontStyle = FontStyle(R.font.cavdreams))
-
-
+fun GoogleSignUp() {
+    // Icon Button
+    // Icon on the Right of text
+    Button(onClick = {}, modifier = Modifier.padding(8.dp)) {
+        Text(text = "Gmail Sign In")
+        Icon(
+            imageVector = Icons.Default.Email,
+            contentDescription = null,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
 }
