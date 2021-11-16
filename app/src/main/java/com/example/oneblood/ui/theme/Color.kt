4a0d0562d299ee82c99752f0cf4268b797ede8f5
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-16 14:02:13 +0300

    new custom colors

--- app/src/main/java/com/example/oneblood/ui/theme/Color.kt
+++ app/src/main/java/com/example/oneblood/ui/theme/Color.kt
@@ -1,8 +1,10 @@
 package com.example.oneblood.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.example.oneblood.R
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFFDDD8E6)
 val Purple700 = Color(0x5B3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val oneblod = Color(R.color.oneblood)
