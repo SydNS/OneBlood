ec0304605a3bb96282283e19b81cbf064bb4263e
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-17 13:12:41 +0300

    added data for the slides

--- app/src/main/java/com/example/oneblood/screens/OnBoardingScreen.kt
+++ app/src/main/java/com/example/oneblood/screens/OnBoardingScreen.kt
@@ -1,2 +1,34 @@
 package com.example.oneblood.screens
 
+import androidx.compose.runtime.Composable
+import com.example.oneblood.R
+import com.example.oneblood.dataclasses.OnBoardingData
+import java.util.ArrayList
+
+
+val items = ArrayList<OnBoardingData>()
+
+@Composable
+fun OnBoardingScreen() {
+    items.add(
+        OnBoardingData(
+            R.drawable.blood2,
+            "Welcome To OneBlood",
+            "help medical personnel and patients in need of blood donation to look up for someone with the same blood type"
+        )
+    )
+    items.add(
+        OnBoardingData(
+            R.drawable.blood4,
+            "Lend a helping hand",
+            "having a list of donors to select from provides comfort and assurance that the patients in question shall be attended to"
+        )
+    )
+    items.add(
+        OnBoardingData(
+            R.drawable.blood6,
+            "All Efforts are notices",
+            "Donors will recieve points from the recipients which they can always redeem and use to acquire health services or even reduce their health services bills"
+        )
+    )
+}
