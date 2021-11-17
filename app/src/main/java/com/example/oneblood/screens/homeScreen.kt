05752ff50e645c9267aa4c752ab5341d5936d7dd
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-17 19:44:51 +0300

    creating an item for showing users in a list

--- app/src/main/java/com/example/oneblood/screens/homeScreen.kt
+++ app/src/main/java/com/example/oneblood/screens/homeScreen.kt
@@ -1,54 +1,28 @@
 package com.example.oneblood.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.oneblood.R
-import com.example.oneblood.dataclasses.BloodCategory
-import com.example.oneblood.dataclasses.bloodCategory
 
+@Preview
 @Composable
-fun homeScreen() {
-
-}
-
-@Composable
-fun ListScreen() {
-    MyList()
-
-}
-
-@Composable
-fun MyList() {
-    LazyColumn {
-        items(items) { item -> ListItem(item) }
-    }
-}
-
-@Composable
-fun ListItem(
-    bloodCategory: BloodCategory,
-    modifier: Modifier =
-        Modifier
-) {
-    Column(modifier = Modifier.padding(8.dp)) {
-        Text(
-            text = bloodCategory.bldname,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold,
-            color = colorResource(id = R.color.oneblood)
-        )
-        Spacer(modifier = modifier.height(8.dp))
+fun Personitem() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(3.dp)) {
+        Image(
+            modifier = Modifier.height(150.dp).width(150.dp)
+                .clip(CircleShape)
+            ,painter = painterResource(id = R.drawable.blood2)
+            , contentDescription ="image to display" )
 
     }
+
 }
