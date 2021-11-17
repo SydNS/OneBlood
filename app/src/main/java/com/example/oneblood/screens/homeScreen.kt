ed4b8f5c0b88c960b17d3dc9832786d6ffbf3d08
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-17 18:23:37 +0300

    item to recycle

--- app/src/main/java/com/example/oneblood/screens/homeScreen.kt
+++ app/src/main/java/com/example/oneblood/screens/homeScreen.kt
@@ -1,7 +1,21 @@
 package com.example.oneblood.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.oneblood.R
+import com.example.oneblood.dataclasses.BloodCategory
+import com.example.oneblood.dataclasses.bloodCategory
 
 @Composable
 fun homeScreen() {
@@ -16,14 +30,25 @@ fun ListScreen() {
 
 @Composable
 fun MyList() {
-//TODO add your code here
+    LazyColumn {
+        items(items) { item -> ListItem(item) }
+    }
 }
 
 @Composable
 fun ListItem(
-    bloodCategory: bloodCategory,
+    bloodCategory: BloodCategory,
     modifier: Modifier =
         Modifier
 ) {
-//TODO add your code here
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(
+            text = bloodCategory.bldname,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.oneblood)
+        )
+        Spacer(modifier = modifier.height(8.dp))
+
+    }
 }
