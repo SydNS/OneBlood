cf0c8428ee48cfa66ccd77128d7c40680ff52a60
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-17 17:22:53 +0300

    creating a  home screen

---
+++ app/src/main/java/com/example/oneblood/screens/homeScreen.kt
@@ -0,0 +1,29 @@
+package com.example.oneblood.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun homeScreen() {
+
+}
+
+@Composable
+fun ListScreen() {
+    MyList()
+
+}
+
+@Composable
+fun MyList() {
+//TODO add your code here
+}
+
+@Composable
+fun ListItem(
+    bloodCategory: bloodCategory,
+    modifier: Modifier =
+        Modifier
+) {
+//TODO add your code here
+}
