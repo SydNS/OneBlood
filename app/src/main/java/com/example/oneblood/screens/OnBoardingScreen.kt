1f2d89ea5db2fcc88274dfc97520616daae4415e
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-17 14:30:53 +0300

    designed the indicator

--- app/src/main/java/com/example/oneblood/screens/OnBoardingScreen.kt
+++ app/src/main/java/com/example/oneblood/screens/OnBoardingScreen.kt
@@ -1,14 +1,19 @@
 package com.example.oneblood.screens
 
 import androidx.annotation.FloatRange
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.painterResource
@@ -121,11 +126,27 @@ fun OnBoardingScreen(
 
 @Composable
 fun PageIndicator(
-    size:Int,currentPage:Int
-){
+    size: Int, currentPage: Int
+) {
     Row(
-        horizontalArrangement = Arrangement.SpaceBetween
-    )
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.padding(top = 60.dp)
+    ) {
+
+    }
 }
 
+@Composable
+fun Indicators(isSelected: Boolean) {
+    val width = animateDpAsState(targetValue = if (isSelected) 25.dp else 10.dp)
+
+    Box(modifier = Modifier
+        .padding(1.dp)
+        .height(10.dp)
+        .width(width.value)
+        .clip(CircleShape)
+        .background(if (isSelected) Color.Black else Color.Red)
+    )
+
+}
 
