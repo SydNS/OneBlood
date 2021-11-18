171dd26e9d5e29d8c2b786f8612983ac3a958638
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-18 13:56:53 +0300

    concetrating all onboarding details in navigation module

--- app/src/main/java/com/example/oneblood/MainActivity.kt
+++ app/src/main/java/com/example/oneblood/MainActivity.kt
@@ -66,30 +66,6 @@ fun DefaultPreview() {
 @ExperimentalAnimationApi
 @Composable
 fun Navigation() {
-//
-//    val items=ArrayList<OnBoardingData>()
-//    items.add(
-//        OnBoardingData(
-//            R.drawable.blood2,
-//            "Welcome To OneBlood",
-//            "help medical personnel and patients in need of blood donation to look up for someone with the same blood type"
-//        )
-//    )
-//    items.add(
-//        OnBoardingData(
-//            R.drawable.blood4,
-//            "Lend a helping hand",
-//            "having a list of donors to select from provides comfort and assurance that the patients in question shall be attended to"
-//        )
-//    )
-//    items.add(
-//        OnBoardingData(
-//            R.drawable.blood6,
-//            "All Efforts are notices",
-//            "Donors will recieve points from the recipients which they can always redeem and use to acquire health services or even reduce their health services bills"
-//        )
-//    )
-
 
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "splash") {
