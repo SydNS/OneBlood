73a48a5f6f88407167a6cf6fd96d402a20c03bd3
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-18 22:43:09 +0300

    this is the home page for show casing the apps details

---
+++ app/src/main/java/com/example/oneblood/screens/HomeScreen.kt
@@ -0,0 +1,29 @@
+package com.example.oneblood.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HomeScreen() {
+
+    Column(
+        modifier = Modifier
+            .background(
+                color = Color.White, shape = RectangleShape
+            )
+            .fillMaxSize(),
+    ) {
+
+
+
+    }
+}
