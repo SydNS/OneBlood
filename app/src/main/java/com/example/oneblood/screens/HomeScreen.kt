2bdbc3486766e8e4651d7111ac066c8a5b68366f
sydneynsiimbe <sydneynsiimbe@gmail.com>
2021-11-18 22:55:01 +0300

    design the top of the homescreen

--- app/src/main/java/com/example/oneblood/screens/HomeScreen.kt
+++ app/src/main/java/com/example/oneblood/screens/HomeScreen.kt
@@ -1,15 +1,18 @@
 package com.example.oneblood.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.oneblood.ui.theme.oneblod
+import com.example.oneblood.ui.theme.oneblod2
 
 @Preview
 @Composable
@@ -22,8 +25,25 @@ fun HomeScreen() {
             )
             .fillMaxSize(),
     ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            oneblod, oneblod2
+                        ),
+                    ),
+                    shape = RoundedCornerShape(
+                        bottomEnd = 90.dp,
+                        bottomStart = 90.dp
+                    )
+                )
+                .height(350.dp)
+                .fillMaxWidth(),
 
 
+            ) {
 
+        }
     }
 }
